3c5e306983590723db7696525be3346db12cb068
bhagya0555 <bhagyalaxmi.naik@photoninfotech.net>
2014-10-06 15:21:56 +0530

    master2 copy updated.

--- ApiTest/TextViewController.m
+++ ApiTest/TextViewController.m
@@ -29,8 +29,8 @@
 {
     self.textView.text=self.content;
     [super viewDidLoad];
-    NSLog(@"In temporary copy1");
-    NSLog(@"In temporary copy2");
+    NSLog(@"In master copy2");
+ 
 
 
 	// Do any additional setup after loading the view.
